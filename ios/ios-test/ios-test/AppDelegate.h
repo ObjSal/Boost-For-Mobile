77bc8d5f5f5f1edf83f8b4d9ce2efcdf16384eec
Salvador Guerrero <salvador.icc@gmail.com>
2016-03-10 01:52:12 -0700

    ofxiOSBoost submodule added

---
+++ ios/ios-test/ios-test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ios-test
+//
+//  Created by Salvador Guerrero on 3/9/16.
+//  Copyright © 2016 ByteApps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ios/ios-test/ios-test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ios-test
+//
+//  Created by Salvador Guerrero on 3/9/16.
+//  Copyright © 2016 ByteApps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
